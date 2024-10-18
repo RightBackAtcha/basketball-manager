7d713207d16cf4e022c77ef637a1c1f4c9a88f33
Jordan Smith <13649267+RightBackAtcha@users.noreply.github.com>
2024-10-17 20:48:57 -0700

    Fix for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,34 +2,21 @@ const CACHE_NAME = 'basketball-manager-cache-v1'
 const assetsToCache = [
     '/',
     '/data',
+    '/favicon',
+    '/images',
     '/generation',
     '/api',
-    '/fonts'
+    '/fonts',
+    '/components',
+    '/workers'
 
 ]
-
-const cacheClone = async (e) => {
-    try {
-        // Fetch cache from network
-        const res = await fetch(e.request);
-        const resClone = res.clone();
-
-        // Open cache and store clone response
-        const cache = await caches.open(CACHE_NAME);
-        await cache.put(e.request, resClone);
-
-        // Return original response
-        return res;
-    } catch(error) {
-        console.error(`Fetch failed: Returning cached resource if available:`, error);
-    }
-};
 
 // Install event for cache
 self.addEventListener('install', (event) => {
     console.log('service worker installed');
     event.waitUntil(
-        cache.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(assetsToCache); // Precache assets
         })
     )
@@ -42,9 +29,9 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map((CACHE_NAME) => {
-                    if (!cacheWhitelist.includes(CACHE_NAME)) {
-                        return caches.delete(CACHE_NAME);
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
                     }
                 })
             )
@@ -54,14 +41,13 @@ self.addEventListener('activate', (event) => {
 
 // Fetch event
 self.addEventListener('fetch', (event) => {
-    event.responseWith(
-        fetch(event.request) // Check cache for request
-            .then((response) => {
-                return caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, response.clone()); // Use cache
-                    return response;
-                });
-            }).catch(() => caches.match(event.request)) // Fallback to cache if network not available
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
     );
 
 });
